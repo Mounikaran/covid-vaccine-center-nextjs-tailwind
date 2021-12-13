a54dea216c9f242d94108545b470a5ddded34be4
mounikaran-r <mounikaran.r@increscotech.com>
2021-12-13 12:23:36 +0530

    slots color change bg-pink-300

--- components/PrintData.js
+++ components/PrintData.js
@@ -17,7 +17,7 @@ function PrintData({ sessions }) {
                 <h2 className="font-bold font-sans text-xl"> Slots </h2> 
               {session.slots.map((slot, index) => (
                 <p key={index} className="text-center my-1">
-                  <span className="rounded-xl bg-yellow-200 px-1">{slot}</span>
+                  <span className="rounded-xl bg-pink-300 px-1">{slot}</span>
                 </p>
               ))}
             </div>
